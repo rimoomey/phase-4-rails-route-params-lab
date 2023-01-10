342db7c1531fe974f97feb4b17a543456957930b
Rimon Moomey <rimon.moomey@gmail.com>
2023-01-10 09:07:29 -0600

    complete lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,10 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
   end
 
+  def show
+    render json: Student.find(params[:id])
+  end
 end
